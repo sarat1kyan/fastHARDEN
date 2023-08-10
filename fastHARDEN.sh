e1254fba55f02549caa9e7047dfee17fdccd3efe
saratikyan <91123731+54R4T1KY4N@users.noreply.github.com>
2023-08-11 01:14:00 +0400

    Update fastHARDEN.sh

--- fastHARDEN.sh
+++ fastHARDEN.sh
@@ -46,194 +46,177 @@ banner() {
 }
 banner 
 
-# Check if the user is root
+#!/bin/bash
+
+# Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
-   banner1 "This script must be run as root"
-   exit 1
-fi
-
-
-# Function to check if a service is running
-is_service_running() {
-    service=$1
-    systemctl is-active --quiet $service
-    return $?
-}
-
-# Function to check if a command is available
-is_command_available() {
-    command=$1
-    type -P $command >/dev/null 2>&1
-    return $?
-}
-
-# Function to disable user creation
-disable_user_creation() {
-    echo "auth required pam_succeed_if.so quiet_success user != root" >> /etc/pam.d/common-auth
-    echo "auth required pam_succeed_if.so quiet_success user != root" >> /etc/pam.d/common-account
-    echo "auth required pam_succeed_if.so quiet_success user != root" >> /etc/pam.d/common-password
-    echo "auth required pam_succeed_if.so quiet_success user != root" >> /etc/pam.d/common-session
-}
-
-# Function to disable privilege changes
-disable_privilege_changes() {
-    chattr +i /etc/passwd
-    chattr +i /etc/shadow
-    chattr +i /etc/group
-    chattr +i /etc/sudoers
-}
-
-# Function to disable kernel file modifications
-disable_kernel_file_modifications() {
-    chattr +i /boot/vmlinuz*
-    chattr +i /boot/initrd.img*
-}
-
-# Function to check if user creation is disabled
-is_user_creation_disabled() {
-    grep -q "auth required pam_succeed_if.so quiet_success user != root" /etc/pam.d/common-auth
-    return $?
-}
-
-# Function to check if privilege changes are disabled
-is_privilege_changes_disabled() {
-    lsattr -d /etc/passwd | grep -q "i"
-    return $?
-}
-
-# Function to check if kernel file modifications are disabled
-is_kernel_file_modifications_disabled() {
-    lsattr -d /boot/vmlinuz* | grep -q "i"
-    return $?
-}
-
-# Check available firewall packages
-if is_command_available "ufw"; then
-    firewall_cmd="ufw"
-    firewall_reload_cmd="ufw reload"
-    firewall_allow_cmd="ufw allow"
-elif is_command_available "firewall-cmd"; then
-    firewall_cmd="firewall-cmd"
-    firewall_reload_cmd="firewall-cmd --reload"
-    firewall_allow_cmd="firewall-cmd --add-port"
-elif is_command_available "iptables"; then
-    firewall_cmd="iptables"
-    firewall_reload_cmd="iptables-save | sudo tee /etc/iptables/rules.v4"
-    firewall_allow_cmd="iptables -A INPUT -p"
-else
-    echo "No supported firewall package found. Exiting..."
+    echo "This script must be run as root"
     exit 1
 fi
 
-# Function to allow a port in the firewall
-allow_port() {
-    port=$1
-    protocol=$2
-    inbound_outbound=$3
-    zone=$4
-    $firewall_allow_cmd=$port/$protocol --$inbound_outbound --zone=$zone >/dev/null 2>&1
+# Function to check if a command is available
+is_command_available() {
+    type -P $1 >/dev/null 2>&1
+    return $?
 }
 
-# Get the list of currently running services
-running_services=$(systemctl list-units --type=service --state=running --no-legend | awk '{print $1}')
+# Function to enable and start a service
+enable_and_start_service() {
+    service=$1
+    systemctl enable --quiet $service
+    systemctl start --quiet $service
+}
 
-# Guide for the user
-echo "This script will monitor running services, open ports, and enforce security measures."
-echo "It will prevent new services from starting, close all other ports, and apply security restrictions."
-echo "You will be prompted to enter the ports you want to open."
-echo "To finish entering ports, enter 'd'."
-echo "Press Ctrl+C to stop the script."
+# Function to allow a port in UFW
+allow_port_in_ufw() {
+    port=$1
+    ufw allow $port >/dev/null 2>&1
+}
 
-# Variables for storing ports
-declare -a ports_array
+# Function to restrict root access
+restrict_root_access() {
+    echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+    echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+    service ssh restart
+}
 
-# Start the loop
-while true; do
-    # Disable new services
-    for service in $running_services
-    do
-        systemctl mask --quiet --now $service
-    done
+# Function to secure sensitive system files
+secure_sensitive_files() {
+    chmod 600 /etc/shadow
+    chmod 600 /etc/gshadow
+    chmod 644 /etc/passwd
+    chmod 644 /etc/group
+    chmod 640 /etc/sudoers
+    chmod 600 /etc/ssh/ssh_host_*key*
+    chmod 600 /root/.ssh/authorized_keys
+    chmod 700 /root
+    chmod 700 /var/log
+}
 
-    # Disable new user creation
-    if ! is_user_creation_disabled; then
-        disable_user_creation
-    fi
+# Function to configure network intrusion detection
+configure_network_intrusion_detection() {
+    apt install snort -y
+    echo "alert icmp any any -> \$HOME_NET any (msg:\"ICMP traffic detected\"; sid:10000001;)" > /etc/snort/rules/local.rules
+    systemctl enable snort
+    systemctl start snort
+}
 
-    # Disable privilege changes
-    if ! is_privilege_changes_disabled; then
-        disable_privilege_changes
-    fi
+# Function to set up log monitoring
+set_up_log_monitoring() {
+    apt install logwatch -y
+    cp /usr/share/logwatch/default.conf/logwatch.conf /etc/logwatch/conf/logwatch.conf
+    echo "MailTo = your_email@example.com" >> /etc/logwatch/conf/logwatch.conf
+}
 
-    # Disable kernel file modifications
-    if ! is_kernel_file_modifications_disabled; then
-        disable_kernel_file_modifications
-    fi
+# Function to secure the /tmp directory
+secure_tmp_directory() {
+    sed -i 's/\/tmp[[:space:]]/\/tmp[[:space:]]\/tmpfs[[:space:]]defaults,nodev,nosuid,noexec[[:space:]]0[[:space:]]0/' /etc/fstab
+    mount -o remount /tmp
+}
 
-    # Disable new cron tasks
-    crontab -r
+# Function to configure system logging
+configure_system_logging() {
+    apt install auditd -y
+    systemctl enable auditd
+    systemctl start auditd
+}
 
-    # Close all other ports
-    if [[ "$firewall_cmd" == "ufw" ]]; then
-        ufw --force reset >/dev/null 2>&1
-        ufw default deny incoming >/dev/null 2>&1
-        ufw default deny outgoing >/dev/null 2>&1
-        ufw --force enable >/dev/null 2>&1
-    else
-        for port in $(ss -nlt | awk 'NR > 1 {print $4}')
-        do
-            if ! [[ "${allowed_ports[@]}" =~ "${port}" ]]; then
-                IFS='/' read -ra port_info <<< "$port"
-                protocol="${port_info[1]}"
-                port_number="${port_info[0]}"
-                $firewall_cmd --zone=public --remove-port=$port_number/$protocol --permanent >/dev/null 2>&1
-            fi
-        done
-        $firewall_reload_cmd
-    fi
+# Function to configure network security
+configure_network_security() {
+    systemctl disable avahi-daemon
+    systemctl disable cups
+    ufw allow 80/tcp
+    ufw allow 443/tcp
+    ufw allow 53/udp
+    ufw allow 123/udp
+    ufw --force enable
+    echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+    echo "net.ipv4.conf.all.accept_redirects = 0" >> /etc/sysctl.conf
+    echo "net.ipv4.conf.default.accept_redirects = 0" >> /etc/sysctl.conf
+    echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
+    echo "net.ipv4.conf.default.send_redirects = 0" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+    sysctl -p
+}
 
-    # Allow specified ports
-    for port_data in "${ports_array[@]}"
-    do
-        IFS='/' read -ra port_info <<< "$port_data"
-        port_number="${port_info[0]}"
-        protocol="${port_info[1]}"
-        inbound_outbound="${port_info[2]}"
-        zone="${port_info[3]}"
-        allow_port $port_number $protocol $inbound_outbound $zone
-    done
+# Function to enforce password policies
+enforce_password_policies() {
+    apt install libpam-pwquality -y
+    echo "password requisite pam_pwquality.so retry=3" >> /etc/pam.d/common-password
+    echo "password requisite pam_pwquality.so minlen=12" >> /etc/pam.d/common-password
+    echo "password requisite pam_pwquality.so ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1" >> /etc/pam.d/common-password
+    echo "password requisite pam_pwquality.so minclass=3" >> /etc/pam.d/common-password
+}
 
-    clear
-    echo "List of running services:"
-    for service in $running_services
-    do
-        if is_service_running $service; then
-            echo "$service is running"
-        else
-            echo "$service is not running"
-        fi
-    done
+# Function to harden SSH configuration
+harden_ssh() {
+    echo "Protocol 2" >> /etc/ssh/sshd_config
+    echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+    echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+    echo "AllowUsers your_username" >> /etc/ssh/sshd_config
+    service ssh restart
+}
 
-    echo "List of open ports:"
-    for port in "${allowed_ports[@]}"
-    do
-        IFS='/' read -ra port_info <<< "$port"
-        protocol="${port_info[1]}"
-        port_number="${port_info[0]}"
-        if is_command_available "ss"; then
-            if ss -nlt | grep -q "$port_number.*$protocol"; then
-                echo "$port is open"
-            else
-                echo "$port is closed"
-            fi
-        else
-            if netstat -tuln | grep -q "$port_number.*$protocol"; then
-                echo "$port is open"
-            else
-                echo "$port is closed"
-            fi
-        fi
-    done
+# Function to restrict access to system binaries
+restrict_access_to_binaries() {
+    chmod 750 /bin/*
+    chmod 750 /usr/bin/*
+    chmod 750 /sbin/*
+    chmod 750 /usr/sbin/*
+}
 
-    sleep 5
-done
+# Function to implement process monitoring
+implement_process_monitoring() {
+    apt install psacct -y
+    systemctl enable psacct
+    systemctl start psacct
+}
+
+# Function to disable unnecessary services
+disable_unnecessary_services() {
+    systemctl disable bluetooth
+    systemctl disable cups
+    systemctl disable cups-browsed
+    systemctl disable avahi-daemon
+    systemctl disable avahi-daemon.socket
+}
+
+# Function to configure kernel parameters
+configure_kernel_parameters() {
+    echo "kernel.exec-shield = 1" >> /etc/sysctl.conf
+    echo "kernel.randomize_va_space = 2" >> /etc/sysctl.conf
+    echo "net.ipv4.ip_forward = 0" >> /etc/sysctl.conf
+    sysctl -p
+}
+
+# Function to enforce strong user authentication
+enforce_strong_user_authentication() {
+    echo "auth required pam_tally2.so deny=5 unlock_time=900" >> /etc/pam.d/common-auth
+    echo "auth required pam_unix.so nullok_secure use_first_pass" >> /etc/pam.d/common-auth
+    echo "auth required pam_tally2.so onerr=succeed file=/var/log/tallylog" >> /etc/pam.d/common-account
+}
+
+# Apply all security measures
+apply_all_security_measures() {
+    restrict_root_access
+    secure_sensitive_files
+    configure_network_intrusion_detection
+    set_up_log_monitoring
+    secure_tmp_directory
+    configure_system_logging
+    configure_network_security
+    enforce_password_policies
+    harden_ssh
+    restrict_access_to_binaries
+    implement_process_monitoring
+    disable_unnecessary_services
+    configure_kernel_parameters
+    enforce_strong_user_authentication
+}
+
+# Apply security measures
+apply_all_security_measures
+
+# Notify user that the script has completed
+echo "System hardening completed."
